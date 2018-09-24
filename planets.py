815e046d42739b735888b5aff17cea966cc4ce14
Ryan Madamba <ryanmadamba@Ryans-MacBook-Pro.local>
2018-09-24 15:19:45 -0700

    Commit 2 for Lab0

--- planets.py
+++ planets.py
@@ -1,3 +1,11 @@
+#
+# Name: Ryan Madamba
+# Student ID: 014684341
+# 9/24/18
+# Lab 0
+# Section 15
+# Prompts user what their weight is and will RETURN their weight on Mars and Jupiter
+
 def weight_on_planets():
    weight = int(input("What do you weigh on earth? "))
    mars = weight * 0.38
